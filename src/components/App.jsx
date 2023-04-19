604e7507b44e442eb7e5204d214a6123b54b48a3
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2023-04-19 20:26:11 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,13 @@ export function App() {
   const contact = localStorage.getItem('contacts');
   const contactsParse = JSON.parse(contact);
   const [contacts, setContacts] = useState(
-    contactsParse ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
+    () =>
+      contactsParse ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
   );
   const [filter, setFilter] = useState('');
 
